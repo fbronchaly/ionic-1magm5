163bb1d597ba02b435e7813e1d88fe66978f6fc7
fbronchaly <fbronchaly@gmail.com>
2020-04-04 21:03:44 +0200

    3 files modified, 3 added

---
+++ pages/agua/agua.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agua',
+  templateUrl: './agua.component.html',
+  styleUrls: ['./agua.component.css']
+})
+export class AguaComponent  {
+
+  constructor() { }
+
+  
+
+}
