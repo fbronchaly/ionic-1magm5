b7f734cf51e69a59d710a545d13ddb8217f777e5
fbronchaly <fbronchaly@gmail.com>
2020-04-06 19:49:36 +0200

    3 files modified

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,5 +1,4 @@
 import { NgModule, ErrorHandler } from '@angular/core';
-import { Calendar } from '@ionic-native/calendar/ngx';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
 import { MyApp } from './app.component';
@@ -25,7 +24,7 @@ import { TabsPage } from '../pages/tabs/tabs';
   imports: [
     BrowserModule,
     IonicModule.forRoot(MyApp),
-    Calendar
+   
   ],
   bootstrap: [IonicApp],
   entryComponents: [
